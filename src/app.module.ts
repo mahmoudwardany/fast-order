918d21737a6e1eb3e4831a5e81d82f89e5920032
shoroukshorouk <mahmoudwardany18@gmail.com>
2025-05-31 01:10:05 +0300

    chore(app): setup env config service - create env folder and add environment-specific .env files and example - add .env and env/* to .gitignore - implement ConfigService to load .env files - inject ConfigService using useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { ConfigModule } from './config/config.module';
+
+dotenv.config({
+  path: path.join(process.cwd(), 'env', `${process.env.NODE_ENV?.trim()}.env`),
+});
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  const configService = app.select(ConfigModule).get(ConfigService);
+  const port = configService.PORT;
+  console.log(`app is listen at ${port}`);
 }
+
 bootstrap();
