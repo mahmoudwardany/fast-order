46e1502ce76400911b490d080ceedb9b84caf9d9
shoroukshorouk <mahmoudwardany18@gmail.com>
2025-06-19 22:04:56 +0300

    feat(core): implement global exception filter for unified error responses
    - Created a GlobalFilter class to handle all uncaught exceptions across the app
    - Ensures consistent error structure with statusCode, message, request URL, and timestamp
    - Handles both HttpException and generic Error instances gracefully

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './modules/config/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalFilter } from './filters/global-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalFilter());
+
   const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const port = configService.get('PORT');
+
   await app.listen(port);
   console.log(`🚀 App is running on http://localhost:${port}`);
 }
